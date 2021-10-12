151568b08d03d8f9c49d1a09fef87ae40d13b993
russellmatt66 <47687321+russellmatt66@users.noreply.github.com>
2021-10-11 20:24:59 -0700

    Problem 2 chugging
    import numpy as np
    ModuleNotFound but conda is in PATH

---
+++ hw1/aa530-hw1.py
@@ -0,0 +1,13 @@
+"""
+Matt Russell
+AA 530: Solid Mechanics
+10/11/21
+"""
+import numpy as np
+from np.linalg import eig
+
+""" Problem 2.5 """
+LST_2pt5 = np.array([[0.0,0.0,0.0],[0.0,0.0,0.25],[0.0,0.25,0.125]])
+evals, evecs = eig(LST_2pt5)
+print("The eigenvalues of the LST in 2.5 are %f", evals)
+print("The eigenvectors of the LST in 2.5 are %f", evecs)
