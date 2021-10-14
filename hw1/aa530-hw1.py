91160d660a05eda1a529516bd517f84d834b5d5c
russellmatt66 <47687321+russellmatt66@users.noreply.github.com>
2021-10-13 22:49:30 -0700

    Holy done

--- hw1/aa530-hw1.py
+++ hw1/aa530-hw1.py
@@ -8,5 +8,5 @@ import numpy as np
 """ Problem 2.5 """
 LST_2pt5 = np.array([[0.0,0.0,0.0],[0.0,0.0,0.25],[0.0,0.25,0.125]])
 evals, evecs = np.linalg.eig(LST_2pt5)
-print("The eigenvalues of the LST in 2.5 are %f", evals)
-print("The eigenvectors of the LST in 2.5 are %f", evecs)
+print("The eigenvalues of the LST in 2.5 are", evals)
+print("The eigenvectors of the LST in 2.5 are", evecs)
