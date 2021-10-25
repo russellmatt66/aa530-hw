b5da8d2787a836359bcea90856f619450989dd19
russellmatt66 <russellmatt66@gmail.com>
2021-10-25 12:00:51 -0700

    1.6 complete

--- hw2/aa530-hw2.py
+++ hw2/aa530-hw2.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 """ Problem 1 """
-# 1.3 
+# 1.3 and 1.4
 alpha = np.linspace(1.0,179.0,100) # degrees
 sigma_1 = 30.0 # [MPa]
 sigma_2 = 10.0 # [MPa]
@@ -22,5 +22,17 @@ plt.title('Problem 1.3 and 1.4')
 plt.xlim([0.0,180.0])
 plt.xlabel('$\\alpha$ (deg)$')
 plt.legend()
+
+# 1.6
+alpha = np.linspace(0.0,180.0,100) 
+alpharad = np.deg2rad(alpha) # angle in radians for convenience
+Tn = (1.0/2.0)*(sigma_1 + sigma_2) + 0.5*(sigma_1 - sigma_2)*np.cos(2.0*alpharad) + 0.5*(tau_12 + tau_21)*np.sin(2.0*alpharad)
+Ttau = 0.5*(sigma_1 - sigma_2)*np.sin(2.0*alpharad) - 0.5*(tau_12 + tau_21)*np.cos(2.0*alpharad)
+
+fig_stress1pt6 = plt.figure()
+plt.plot(Tn,Ttau)
+plt.title('Stress Trajectory')
+plt.xlabel('Normal Stress [MPa]')
+plt.ylabel('Shear Stress [MPa]')
 
 plt.show()
